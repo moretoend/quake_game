8749f713333b9dee2d33b6ba6ff94eb966d5c93f
Daniel Moreto <dfmoreto@gmail.com>
2024-03-23 23:35:41 -0300

    test: remove unused spec

--- spec/quake_game_spec.rb
+++ spec/quake_game_spec.rb
@@ -4,8 +4,4 @@ RSpec.describe QuakeGame do
   it 'has a version number' do
     expect(QuakeGame::VERSION).not_to be nil
   end
-
-  it 'does something useful' do
-    expect(false).to eq(true)
-  end
 end
